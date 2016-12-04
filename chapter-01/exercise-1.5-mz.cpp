623148415ea54e37ba5c29139dac52dcca0d85d3
Michael Zingale <michael.zingale@stonybrook.edu>
2016-12-04 13:58:38 -0500

    Mike's progress

---
+++ chapter-01/exercise-1.5-mz.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+
+  double x = 0.0, y = 0.0;
+
+  std::cout << "Enter two numbers: " << std::endl;
+
+  std::cin >> x >> y;
+
+  std::cout << "product: " << x*y << std::endl;
+
+}
