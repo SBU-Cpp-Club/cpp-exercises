e000d330575def967ef9bd9e8e3493266d0283cb
F. D. Swesty <douglas.swesty@stonybrook.edu>
2016-12-05 11:11:22 -0500

    Added Doug's solution to exercise 1.3

---
+++ chapter-01/exercise-1.3-ds.cpp
@@ -0,0 +1,16 @@
+//
+//  Purpose:   Solution to exercise 1.3
+//  Author:    D. Swesty
+//  Date:      12/5/2016
+//
+//  Notes:     This program print Hello, World on STDOUT
+//
+#include <iostream>
+
+int main() {
+
+  std::cout << "Hello, World" << std::endl;
+
+  return 0;     // Return a value of 0 to the shell
+
+} 
