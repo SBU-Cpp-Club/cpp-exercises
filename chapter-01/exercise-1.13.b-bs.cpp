4ba3ea2b67b35e3e0d6198365a1e11b43f752918
mbarrios <maria.barriossazo@stonybrook.edu>
2016-12-04 19:46:18 -0500

    some of the for exercises

---
+++ chapter-01/exercise-1.13.b-bs.cpp
@@ -0,0 +1,16 @@
+/*
+ * Print numbers from 10 down to 0 using a for loop and --
+ * */
+
+#include <iostream>
+
+int main()
+{
+
+    for (int val = 10; val >= 0; --val){
+        std::cout << val << std::endl ;
+    }
+
+    return 0;
+
+}
