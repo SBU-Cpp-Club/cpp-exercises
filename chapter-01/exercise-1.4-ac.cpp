e5a572389d12b40c4d376f771ca85a1e21121ade
Alan Calder <alan.calder@stonybrook.edu>
2016-12-09 12:07:49 -0500

    Notes on namespace usage in this example.

--- chapter-01/exercise-1.4-ac.cpp
+++ chapter-01/exercise-1.4-ac.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 //why do we not need the line above?
+//ans- can do it this way, but then don't use std:cout, etc.
+// As per discussion, if you do this style, only do it for std, not others.
+// This is a "using directive."
+// using std::cout  this is a "using declaration" where you are individually declairing each
+// thing you are importing.
 //yes, I know we haven't seen comments yet.
 
 int main(){
