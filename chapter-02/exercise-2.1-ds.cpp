be640c86669c46691c1950d9eba45f30a2fee1f9
F. D. Swesty <douglas.swesty@stonybrook.edu>
2016-12-27 15:38:20 -0500

    Doug's solution to exercise 2.1

---
+++ chapter-02/exercise-2.1-ds.cpp
@@ -0,0 +1,17 @@
+int main() {
+  //
+  //  Integer:  C++11 standard specifies:
+  //     short:      minimum of 16 bits
+  //     int:        minimum of 16 bits & at least as long as short
+  //     long:       minimum of 32 bits & at least as long as int
+  //     long long:  minimum of 64 bits & at least as long as long
+  //
+  //  Unsigned Integer types:
+  //     Designated with "unsigned" keyword before type.  May not be negative.
+  //
+  //  Floating point:  C++11 standard specifies
+  //     float:      minimum of 6 significant digits
+  //     double:     minimum of 10 significant digits
+
+  return 0;
+}
