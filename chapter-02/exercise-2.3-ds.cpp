558636105a12cfcc940ca67575035f49d638b752
F. D. Swesty <douglas.swesty@stonybrook.edu>
2016-12-27 15:50:32 -0500

    Added Doug's solution to exercise 2.3

---
+++ chapter-02/exercise-2.3-ds.cpp
@@ -0,0 +1,24 @@
+//
+//  Purpose:   Implement solution to exercise 2.3
+//  Author:    F. D. Swesty
+//  Date:      12/27/2016 
+#include <iostream>
+using namespace std;
+int main() {
+
+  unsigned u=10, u2=42;
+  int i=10, i2=42;
+
+
+  cout << u2-u << endl;
+  cout << u-u2 << endl;
+
+  cout << i2-i << endl;
+  cout << i-i2 << endl;
+
+  cout << i-u << endl;
+  cout << u-i << endl;
+
+
+  return 0;
+}
