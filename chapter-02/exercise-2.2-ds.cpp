74d744e1a3c84d8570f7313694ab9188f24dc169
F. D. Swesty <douglas.swesty@stonybrook.edu>
2016-12-27 15:44:08 -0500

    Doug's solution to exercise 2.2

---
+++ chapter-02/exercise-2.2-ds.cpp
@@ -0,0 +1,16 @@
+int main() {
+  //
+  //  To calculate a mortgage payment use the following types:
+  //
+  //  For rate:   double.   The rate could be fractional thus making difficult
+  //                        to represent as an integer.   Use double instad of 
+  //                        float in order to achieve maximum precision.
+  //
+  //  For interest: double.   The amount of interest could be fractional
+  //                          depending on the units used.
+  //
+  //  For principal: double.   The amount of principal could be fractional
+  //                          depending on the units used.
+
+  return 0;
+}
