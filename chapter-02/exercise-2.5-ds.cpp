23b511153bcee38020473beaa9322a8ac4dccd13
F. D. Swesty <douglas.swesty@stonybrook.edu>
2017-01-03 17:02:14 -0500

    Added Doug's solution to exercise 2.5

---
+++ chapter-02/exercise-2.5-ds.cpp
@@ -0,0 +1,26 @@
+//
+//  Purpose:   Implement solution to exercise 2.4
+//  Author:    F. D. Swesty
+//  Date:      12/27/2016 
+
+// a:   'a' is a char
+//      L'a' is a wide char
+//      "a" is a C-string
+//      L"a" is a wide C-string
+//
+// b:   10 is an int
+//      10u is an unsigned int
+//      10L is a long int
+//      10uL is an unsigned long int
+//      012  is an int specifed in octal
+//      0xC  is an int specified in hexadecimal
+//
+// c:   3.14 is a double
+//      3.14f is a float
+//      3.14L is a long double
+//
+// d:   10 is an int
+//      10u is an unsigned int
+//      10. is a double
+//      10e-2 is a double
+ 
