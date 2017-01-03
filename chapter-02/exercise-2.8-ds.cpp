f166d183959ecd5cb92edd460ab2a025bd44f2c1
F. D. Swesty <douglas.swesty@stonybrook.edu>
2017-01-03 17:23:34 -0500

    Added Doug's solutions to exercise 2.7 & 2.8

---
+++ chapter-02/exercise-2.8-ds.cpp
@@ -0,0 +1,15 @@
+//
+//  Purpose:   Implement solution to exercise 2.8
+//  Author:    F. D. Swesty
+//  Date:      1/3/2017 
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+
+
+  cout << "2\tM\n";
+
+
+  return 0;
+}
