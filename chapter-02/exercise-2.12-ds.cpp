39d7d50e58e6248f11a4195673f0bf06f6b9e42b
F. D. Swesty <douglas.swesty@stonybrook.edu>
2017-01-06 13:26:17 -0500

    Added  Doug's solution to exercise 2.12

---
+++ chapter-02/exercise-2.12-ds.cpp
@@ -0,0 +1,23 @@
+//
+//  Purpose:   Implement solution to exercise 2.12
+//  Author:    F. D. Swesty
+//  Date:      1/6/2017 
+
+#include <iostream>
+
+int main() {
+
+  //  int double = 3.14;    // Illegal name -- conflicts with keywords
+
+  //  int catch-22;         // Illegal name -- no dashes allowed
+
+  //  double Double = 3.14; // Legal
+
+  // int _;                 // Legal
+
+  // int 1_or_2 = 1;        // Illegal name -- identifier cannot 
+                            // begin with a digit
+
+  return 0;
+
+}
