40672e6355b67fa5a7f0006c24eb3fb9b69111e6
F. D. Swesty <douglas.swesty@stonybrook.edu>
2017-01-06 13:59:43 -0500

    Addd Doug's solution to exercise 2.13

---
+++ chapter-02/exercise-2.13-ds.cpp
@@ -0,0 +1,22 @@
+//
+//  Purpose:   Implement solution to exercise 2.13
+//  Author:    F. D. Swesty
+//  Date:      1/6/2017 
+//
+//  Note:      This program will return j=100;
+//
+#include <iostream>
+
+int i = 42;
+
+int main() {
+
+  int i=100;
+
+  int j = i;
+
+  std::cout << j << std::endl;
+
+  return 0;
+
+}
