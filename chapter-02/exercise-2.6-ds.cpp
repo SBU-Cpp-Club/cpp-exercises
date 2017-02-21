83d7a574c49ef2578ea2c48b1fe5ed159b3941ab
F. D. Swesty <douglas.swesty@stonybrook.edu>
2017-02-21 13:41:34 -0500

    Doug's solution to exercis 2.6 updated

--- chapter-02/exercise-2.6-ds.cpp
+++ chapter-02/exercise-2.6-ds.cpp
@@ -8,9 +8,9 @@ int main(){
 
   int month=9, dat=7;  // This statement assigns integers 9 & 7 to variables
 
-  int month=09, day=07; // This statement is illegal
-                        // 07 is a legal octal integer literal
-                        // 09 is an illegal octal integer literal
+  //  int month=09, day=07; // This statement is illegal
+                            // 07 is a legal octal integer literal
+                            // 09 is an illegal octal integer literal
 
   return 0;
 }
