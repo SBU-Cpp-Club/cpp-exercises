9c50b90e57365bf4bcabb5cd9821a3cdeca02842
F. D. Swesty <douglas.swesty@stonybrook.edu>
2017-02-21 14:42:26 -0500

    Doug's solution to exercis 2.19

---
+++ chapter-02/exercise-2.19-ds.cpp
@@ -0,0 +1,21 @@
+//
+//  Purpose:   Implement solution to exercise 2.19
+//  Author:    F. D. Swesty
+//  Date:      2/21/2017 
+//
+//  Note:  This code describes the difference between pointers and references
+//
+#include <iostream>
+using namespace std;
+int main() {
+
+  // A reference creates an alias for an existing object.  The referrence
+  // is "constant" in the sense that it can only point to a single object.
+  
+
+  // A pointer contains the address of an object.   The pointer can change
+  // at execution time.  
+
+
+  return 0;
+}
