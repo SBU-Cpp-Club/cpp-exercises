8ad796b9955bd6510194ce22269b8a344c72c0c7
F. D. Swesty <douglas.swesty@stonybrook.edu>
2017-02-21 13:40:36 -0500

    Doug's solution to exercis 2.7 updated

--- chapter-02/exercise-2.7-ds.cpp
+++ chapter-02/exercise-2.7-ds.cpp
@@ -2,14 +2,17 @@
 //  Purpose:   Implement solution to exercise 2.7
 //  Author:    F. D. Swesty
 //  Date:      1/3/2017 
+using namespace std;
+int main() {
+  // a:   "Who goes with F\145rgus?\012" is a C-string with value
+  //      "Who goes with Fergus?\n"
 
-// a:   "Who goes with F\145rgus?\012" is a C-string with value
-//      "Who goes with Fergus?\n"
-
-// b:   3.14e1L is a long double with value 31.4
+  // b:   3.14e1L is a long double with value 31.4
 
 
-// c:   1024f is a float with value 1024
+  // c:   1024f is a float with value 1024
 
-// d:   3.14L is a long double with value 3.14
- 
+  // d:   3.14L is a long double with value 3.14
+
+  return 0;
+} 
