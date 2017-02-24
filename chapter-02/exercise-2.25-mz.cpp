d87d2e382fbb132d205f26ff0fbc6d80609a2d62
Michael Zingale <michael.zingale@stonybrook.edu>
2017-02-24 10:30:27 -0500

    more examples

---
+++ chapter-02/exercise-2.25-mz.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+  // here ip is a pointer to an int
+  //      i is an int
+  //      r is a reference to an int
+  int* ip, i, &r = i;
+
+  // here j is an int
+  //      jp is a pointer to an int, initialized to 0
+  int j, *jp = 0;
+
+  // here lp is a pointer to an int
+  //      lp2 is an int
+  int* lp, lp2;
+
+  return 0;
+
+}
