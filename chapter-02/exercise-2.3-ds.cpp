d8af9cded612b96d294459a31aeb4c48aeedfb7b
F. D. Swesty <douglas.swesty@stonybrook.edu>
2017-03-09 14:41:33 -0500

    Added changes to Doug's solution to exercise 2.3

--- chapter-02/exercise-2.3-ds.cpp
+++ chapter-02/exercise-2.3-ds.cpp
@@ -1,17 +1,19 @@
 //
-//  Purpose:   Implement solution to exercise 2.3
+//  Purpose:   Implement solution to exercise 2.4
 //  Author:    F. D. Swesty
 //  Date:      12/27/2016 
 #include <iostream>
 using namespace std;
 int main() {
 
-  unsigned u=10, u2=42;
+  unsigned u=10, u2=42, u3;
   int i=10, i2=42;
 
 
   cout << u2-u << endl;
-  cout << u-u2 << endl;
+  u3 = u-u2;
+  cout << u3 << endl;
+  cout << u3+u2 << endl;
 
   cout << i2-i << endl;
   cout << i-i2 << endl;
