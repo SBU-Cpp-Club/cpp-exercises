02fc78ce711d23800ad79f1b439291dfcc47631c
F. D. Swesty <douglas.swesty@stonybrook.edu>
2017-03-09 14:42:15 -0500

    Added Doug's solution to exercise 2.25

---
+++ chapter-02/exercise-2.25-ds.cpp
@@ -0,0 +1,14 @@
+//
+//  Purpose:   Implement solution to exercise 2.24
+//  Author:    F. D. Swesty
+//  Date:      2/21/2017 
+//
+//
+#include <iostream>
+using namespace std;
+int main() {
+
+  int *p = 0;
+
+  return 0;
+}
