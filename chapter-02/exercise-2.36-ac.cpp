e3216d5c5657c244f4360f6fe4221f6ca86b2f28
Alan Calder <acalder@localhost.localdomain>
2017-03-31 12:32:36 -0400

    Notes added to a couple of problems.

--- chapter-02/exercise-2.36-ac.cpp
+++ chapter-02/exercise-2.36-ac.cpp
@@ -8,7 +8,7 @@ int main()
    int a = 3, b = 4; 
    decltype(a) c = a;    // c is an int
    decltype((b)) d = a;  // parentheses on b make d a reference
-//   decltype((b)) e;  // parentheses on b make d a reference- won't work as e must be initialized
+//   decltype((b)) e;  // parentheses on b make d a reference- won't work as e a refernece must be initialized. Quirk is that the double parentheses is always a reference.
    
    ++c;
    ++d;
--- chapter-02/exercise-2.37-ac.cpp
+++ chapter-02/exercise-2.37-ac.cpp
@@ -7,7 +7,7 @@ int main()
    
    int a = 3, b = 4; 
    decltype(a) c = a;    // c is an int
-   decltype(a=b) d = a;  // assignment makes d a reference
+   decltype(a=b) d = a;  // assignment yields a reference- makes d a reference
 //   decltype(a=b) e;      // assignment makes d a reference illegal
    
    ++c;
