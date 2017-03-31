876ca4c78b9c24714f6b4243cc1dc255f015090c
guadabsb15 <guadabsb15@gmail.com>
2017-03-31 11:03:04 -0400

    section 2.6.1

---
+++ chapter-02/exercise-2.39-bs.cpp
@@ -0,0 +1,7 @@
+
+struct Foo {}; //error: expected ‘;’ after struct definition... when missing ; 
+
+int main()
+{
+   return 0;
+}
---
+++ chapter-02/exercise-2.40-bs.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+struct Sales_data{
+    std::string bookNo;
+    int units_sold = 0;
+    double revenue = 0.0;  
+};
+
+int main()
+{
+  Sales_data accum;
+  return 0;
+}
