138cdab4062ea4b46c22d9c241063f30f647f342
F. D. Swesty <douglas.swesty@stonybrook.edu>
2017-04-07 12:36:12 -0400

    An example of what cin >> x returns

---
+++ chapter-03/couttest.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+  double x = 3.14;
+
+  cout << (cin >> x) << endl;
+  cout << (cin >> x) << endl;
+  cout << "done"  << endl;
+
+  return 0;
+} 
