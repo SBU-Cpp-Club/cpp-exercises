eac1ddf6524c65e42ea8437aa9df8e688322a3f7
F. D. Swesty <douglas.swesty@stonybrook.edu>
2017-04-21 14:09:12 -0400

    Added Doug's solution to exercise 3.2

---
+++ chapter-03/exercise-3.2-ds.cpp
@@ -0,0 +1,32 @@
+//
+//  Purpose:   Implement solution to exercise 3.2
+//  Author:    F. D. Swesty
+//  Date:      4/21/2017 
+//
+//
+#include <iostream>
+#include <string>
+
+using std::cout;   // Bring in cout from the standard namespace
+using std::endl;   // Bring in endl from the standard namespace
+using std::string; // Bring in string from the standard namespace
+using std::cin;    // Bring in cin from standard namespace
+
+
+int main() {
+
+  string line, word;   // Declare instances of string to hold a line & a word
+
+  while( getline(cin,line) ) {
+
+    cout << line << endl;    // Output the line to STDOUT
+
+    if( line == "" ) break;
+
+  }
+
+  while( cin >> word ) {
+    cout << word << endl;          // Output word to STDOUT
+  }
+  return 0;   // Stop
+}
