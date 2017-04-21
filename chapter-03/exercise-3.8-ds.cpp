7fb3832e9cf78fd1da72e17d9d830f606e3f6b4b
F. D. Swesty <douglas.swesty@stonybrook.edu>
2017-04-21 16:08:35 -0400

    Added Doug's solution to exercise 3.9

--- chapter-03/exercise-3.8-ds.cpp
+++ chapter-03/exercise-3.8-ds.cpp
@@ -1,5 +1,5 @@
 //
-//  Purpose:   Implement solution to exercise 3.8
+//  Purpose:   Implement solution to exercise 3.7
 //  Author:    F. D. Swesty
 //  Date:      4/21/2017 
 //
@@ -14,33 +14,17 @@ using std::string; // Bring in string from the standard namespace
 
 int main() {
 
-  string s1("This is a string");   // Declare & initialize instance of string
-  string s2("This is a string");   // Declare & initialize instance of string
-  string s3("This is a string");   // Declare & initialize instance of string
+  string s("This is a string");   // Declare & initialize instance of string
 
-  //   Method 1: using a range for loop
-  cout << " string is: " << s1 << endl;
-  for( auto &c : s1) {
+  cout << " string is: " << s << endl;
+
+  // By defining the loop control variable c to be a char, 
+  // the string WILL NOT be altered.
+  for( char c : s) {
     c = 'X';
     cout << " c = " << c << endl;
   }
-  cout << " string is now: " << s1 << endl;
-
-  //   Method 2: using a traditional for loop
-  cout << " string is: " << s2 << endl;
-  for( decltype(s2.size()) i=0; i < s2.size(); i++) {
-    s2[i] = 'X';
-  }
-  cout << " string is now: " << s2 << endl;
-
-  //   Method 3: using a while loop
-  cout << " string is: " << s3 << endl;
-  decltype(s3.size()) j=0;
-  while( j < s3.size() ) {
-    s3[j] = 'X';
-    j++;
-  }
-  cout << " string is now: " << s3 << endl;
+  cout << " string is still: " << s << endl;
 
   return 0;   // Stop
 }
---
+++ chapter-03/exercise-3.9-ds.cpp
@@ -0,0 +1,22 @@
+//
+//  Purpose:   Implement solution to exercise 3.9
+//  Author:    F. D. Swesty
+//  Date:      4/21/2017 
+//
+//
+#include <iostream>
+#include <string>
+
+using std::cout;   // Bring in cout from the standard namespace
+using std::endl;   // Bring in endl from the standard namespace
+using std::string; // Bring in string from the standard namespace
+
+
+int main() {
+
+  string s;   // Declare instance of string
+
+  cout << s << endl;  // This is valid and outputs an empty string
+
+  return 0;   // Stop
+}
