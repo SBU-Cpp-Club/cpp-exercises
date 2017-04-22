308171a7d022ca40dc80f23e56ffc0d064d8f9ec
Alan Calder <alan.calder@stonybrook.edu>
2017-04-22 11:49:40 -0400

    Cleaned up problem.

--- chapter-03/exercise-3.4-ac.cpp
+++ chapter-03/exercise-3.4-ac.cpp
@@ -6,13 +6,13 @@ using std::string;
 int main(){
    string s1, s2;
 
-   cout << s1 << endl; // see what an uninitialized string looks like
-
    getline(std::cin,s1);
    cout << s1 << endl;
 
    getline(std::cin,s2);
    cout << s2 << endl;
+
+   cout << "First check equality and size(?)" << endl;
 
    if (s1 == s2)
       cout << "strings are equal" << endl;
@@ -23,6 +23,21 @@ int main(){
       cout << s1 << " is greater than " << s2 << endl;
       if (s2 > s1)
       cout << s2 << " is greater than " << s1 << endl;
+      }
+
+   cout << "Now check length" << endl;
+
+      auto len1 = s1.size();
+      auto len2 = s2.size();
+
+   if (len1 == len2)
+      cout << "strings are of same lenght" << endl;
+   else
+      {
+      if (len1 > len2)
+      cout << len1 << " is longer than " << s2 << endl;
+      if (len2 > len1)
+      cout << s2 << " is longer than " << s1 << endl;
       }
 
    return 0;
