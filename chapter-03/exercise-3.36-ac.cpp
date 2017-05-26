ea213094ffeeee9e8c7928219cc47ee9f74105f8
Alan Calder <acalder@localhost.localdomain>
2017-05-26 12:33:50 -0400

    Updated exercise as per discussion at C++ club.

--- chapter-03/exercise-3.36-ac.cpp
+++ chapter-03/exercise-3.36-ac.cpp
@@ -10,6 +10,7 @@ int main()
   int ia[] = {0,1,2, 3, 4, 5, 6, 7, 8, 9};
   int ib[] = {0,1,5, 3, 4, 5, 7, 7, 8, 9};
 
+// doesn't use pointers. How should I have done it?
   for (int i = 0; i < 10; i++)
      if(ia[i] != ib[i])
          cout  << " arrays differ at element " << i << endl;
@@ -29,19 +30,21 @@ int main()
 //   v2.at(2) = 5;
 //   v2.at(6) = 7;
 
-//   int k = 0;
-//   for (auto it = v1.begin(); it != v1.end(); ++it){
-//     if(it != v2[k])
-//         cout  << " vectors differ at element " << k << endl;
-//     ++k;
-//   }
-
-   int j = 0;
-   for (decltype (v1.size()) it = 0; it != 10; ++it){
-     if(it != v2[j])
-         cout  << " vectors differ at element " << j << endl;
-     ++j;
+// with range for
+   decltype (v1.size()) k = 0;
+   for (auto it : v1){
+     if(it != v2[k])
+         cout  << " vectors differ at element " << k << endl;
+     ++k;
    }
+
+// with regular for  (this works, just commented out)
+//   decltype (v1.size()) j = 0;
+//   for (decltype (v1.size()) it = 0; it != 10; ++it){
+//      if(it != v2[j])
+//          cout  << " vectors differ at element " << j << endl;
+//      ++j;
+//    }
 
   return 0;
 }
