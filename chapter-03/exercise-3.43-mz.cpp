ff989a758608b18810acb1a331db553171351fb6
Michael Zingale <michael.zingale@stonybrook.edu>
2017-09-15 11:04:45 -0400

    new exercise

---
+++ chapter-03/exercise-3.43-mz.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+
+  int ia[3][4] = { {0, 1, 2, 3},
+		   {4, 5, 6, 7},
+		   {8, 9, 10, 11}};
+
+  // method 1 : range for
+  for (const int (&row)[4] : ia) {
+    for (const int col : row) {
+      std::cout << col << std::endl;
+    }
+  }
+
+  std::cout << std::endl;
+
+  for (int i=0; i < 3; i++)
+    for (int j=0; j < 4; j++)
+      std::cout << ia[i][j] << std::endl;
+
+  std::cout << std::endl;
+
+  for (int (*p)[4] = ia; p < ia + 3; ++p) {
+    for (int *q = *p; q < *p + 4; ++q) {
+      std::cout << *q << std::endl;
+    }
+  }
+  return 0;
+}
